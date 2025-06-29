6642dbf8112131d60c41b4b11bf6e45514c5c947
Leah Zhou <zhouxy89@yahoo.com>
2025-06-29 10:16:56 +0200

    Update LogItem.swift

--- QuantiBike/Models/LogItem.swift
+++ QuantiBike/Models/LogItem.swift
@@ -1,58 +1,59 @@
-//  LogItem.swift
-//  QuantiBike
-//  Updated for dual foot FSR data logging
-
 import Foundation
 import CoreMotion
 import CoreLocation
 
-struct FootSensorData {
-    var fsr1: Int = 0
-    var fsr2: Int = 0
-    var fsr3: Int = 0
-    var fsr4: Int = 0
-    var fsr1_raw: Int = 0
-    var fsr2_raw: Int = 0
-    var fsr3_raw: Int = 0
-    var fsr4_raw: Int = 0
-    var fsr1_norm: Float = 0
-    var fsr2_norm: Float = 0
-    var fsr3_norm: Float = 0
-    var fsr4_norm: Float = 0
-    var fsr1_baseline: Int = 0
-    var fsr2_baseline: Int = 0
-    var fsr3_baseline: Int = 0
-    var fsr4_baseline: Int = 0
-    var fsr1_max: Int = 1
-    var fsr2_max: Int = 1
-    var fsr3_max: Int = 1
-    var fsr4_max: Int = 1
-}
-
 struct LogItem {
     let timestamp: TimeInterval
-    let phoneBattery: Float
-    let left: FootSensorData
-    let right: FootSensorData
-    let calibrationStatus: String
     let phoneAcceleration: CMAccelerometerData?
     let phoneMotionData: CMDeviceMotion?
+    let phoneBattery: Float
+
+    let leftFoot: FootSensorData
+    let rightFoot: FootSensorData
+
+    let leftCalibrationStatus: String
+    let rightCalibrationStatus: String
     let locationData: CLLocation?
 
     var dictionary: [String: Any] {
         var result: [String: Any] = [
             "timestamp": String(timestamp),
             "phoneBattery": String(phoneBattery),
-            "calibrationStatus": calibrationStatus,
-            "left": sensorDataDict(foot: left),
-            "right": sensorDataDict(foot: right),
+            "leftCalibrationStatus": leftCalibrationStatus,
+            "rightCalibrationStatus": rightCalibrationStatus,
             "acceleration": preparePhoneAcc(),
             "locationData": prepareLocationData(locationData: locationData),
             "unixTimeStamp": String(Date().timeIntervalSince1970)
         ]
 
-        let motionData = prepareMotionData(motionData: phoneMotionData)
-        for (key, value) in motionData {
+        for (prefix, foot) in [("left", leftFoot), ("right", rightFoot)] {
+            result["\(prefix)_heel"] = foot.heel
+            result["\(prefix)_toe"] = foot.toe
+            result["\(prefix)_midLeft"] = foot.midLeft
+            result["\(prefix)_midRight"] = foot.midRight
+
+            result["\(prefix)_heel_raw"] = foot.heel_raw
+            result["\(prefix)_toe_raw"] = foot.toe_raw
+            result["\(prefix)_midLeft_raw"] = foot.midLeft_raw
+            result["\(prefix)_midRight_raw"] = foot.midRight_raw
+
+            result["\(prefix)_heel_norm"] = foot.heel_norm
+            result["\(prefix)_toe_norm"] = foot.toe_norm
+            result["\(prefix)_midLeft_norm"] = foot.midLeft_norm
+            result["\(prefix)_midRight_norm"] = foot.midRight_norm
+
+            result["\(prefix)_heel_baseline"] = foot.heel_baseline
+            result["\(prefix)_toe_baseline"] = foot.toe_baseline
+            result["\(prefix)_midLeft_baseline"] = foot.midLeft_baseline
+            result["\(prefix)_midRight_baseline"] = foot.midRight_baseline
+
+            result["\(prefix)_heel_max"] = foot.heel_max
+            result["\(prefix)_toe_max"] = foot.toe_max
+            result["\(prefix)_midLeft_max"] = foot.midLeft_max
+            result["\(prefix)_midRight_max"] = foot.midRight_max
+        }
+
+        for (key, value) in prepareMotionData(motionData: phoneMotionData) {
             result[key] = value
         }
 
@@ -67,16 +68,6 @@ struct LogItem {
         return String(data: data, encoding: .utf8) ?? ""
     }
 
-    private func sensorDataDict(foot: FootSensorData) -> [String: Any] {
-        return [
-            "fsr1": foot.fsr1, "fsr2": foot.fsr2, "fsr3": foot.fsr3, "fsr4": foot.fsr4,
-            "fsr1_raw": foot.fsr1_raw, "fsr2_raw": foot.fsr2_raw, "fsr3_raw": foot.fsr3_raw, "fsr4_raw": foot.fsr4_raw,
-            "fsr1_norm": foot.fsr1_norm, "fsr2_norm": foot.fsr2_norm, "fsr3_norm": foot.fsr3_norm, "fsr4_norm": foot.fsr4_norm,
-            "fsr1_baseline": foot.fsr1_baseline, "fsr2_baseline": foot.fsr2_baseline, "fsr3_baseline": foot.fsr3_baseline, "fsr4_baseline": foot.fsr4_baseline,
-            "fsr1_max": foot.fsr1_max, "fsr2_max": foot.fsr2_max, "fsr3_max": foot.fsr3_max, "fsr4_max": foot.fsr4_max
-        ]
-    }
-
     func preparePhoneAcc() -> [String: String] {
         return [
             "x": phoneAcceleration?.acceleration.x.description ?? "",
@@ -87,46 +78,35 @@ struct LogItem {
     }
 
     func prepareMotionData(motionData: CMDeviceMotion?) -> [String: Any] {
-        guard let motion = motionData else { return [:] }
-        return [
-            "quaternion": [
-                "x": motion.attitude.quaternion.x.description,
-                "y": motion.attitude.quaternion.y.description,
-                "z": motion.attitude.quaternion.z.description,
-                "w": motion.attitude.quaternion.w.description
-            ],
-            "pitch": motion.attitude.pitch.description,
-            "yaw": motion.attitude.yaw.description,
-            "roll": motion.attitude.roll.description,
-            "timestamp": motion.timestamp.description,
-            "rotationMatrix": [
-                "m11": motion.attitude.rotationMatrix.m11.description,
-                "m12": motion.attitude.rotationMatrix.m12.description,
-                "m13": motion.attitude.rotationMatrix.m13.description,
-                "m21": motion.attitude.rotationMatrix.m21.description,
-                "m22": motion.attitude.rotationMatrix.m22.description,
-                "m23": motion.attitude.rotationMatrix.m23.description,
-                "m31": motion.attitude.rotationMatrix.m31.description,
-                "m32": motion.attitude.rotationMatrix.m32.description,
-                "m33": motion.attitude.rotationMatrix.m33.description
-            ],
-            "userAccel": [
-                "x": motion.userAcceleration.x.description,
-                "y": motion.userAcceleration.y.description,
-                "z": motion.userAcceleration.z.description
-            ],
-            "rotationRate": [
-                "x": motion.rotationRate.x.description,
-                "y": motion.rotationRate.y.description,
-                "z": motion.rotationRate.z.description
-            ],
-            "magneticField": [
-                "x": motion.magneticField.field.x.description,
-                "y": motion.magneticField.field.y.description,
-                "z": motion.magneticField.field.z.description,
-                "accuracy": motion.magneticField.accuracy.rawValue.description
-            ]
+        var motionArr: [String: Any] = [:]
+        motionArr["quaternion"] = [
+            "x": motionData?.attitude.quaternion.x.description ?? "",
+            "y": motionData?.attitude.quaternion.y.description ?? "",
+            "z": motionData?.attitude.quaternion.z.description ?? "",
+            "w": motionData?.attitude.quaternion.w.description ?? ""
         ]
+        motionArr["pitch"] = motionData?.attitude.pitch.description ?? ""
+        motionArr["yaw"] = motionData?.attitude.yaw.description ?? ""
+        motionArr["roll"] = motionData?.attitude.roll.description ?? ""
+        motionArr["timestamp"] = motionData?.timestamp.description ?? ""
+        motionArr["rotationMatrix"] = prepareRotationMatrix(motionData: motionData)
+        motionArr["userAccel"] = [
+            "x": motionData?.userAcceleration.x.description ?? "",
+            "y": motionData?.userAcceleration.y.description ?? "",
+            "z": motionData?.userAcceleration.z.description ?? ""
+        ]
+        motionArr["rotationRate"] = [
+            "x": motionData?.rotationRate.x.description ?? "",
+            "y": motionData?.rotationRate.y.description ?? "",
+            "z": motionData?.rotationRate.z.description ?? ""
+        ]
+        motionArr["magneticField"] = [
+            "x": motionData?.magneticField.field.x.description ?? "",
+            "y": motionData?.magneticField.field.y.description ?? "",
+            "z": motionData?.magneticField.field.z.description ?? "",
+            "accuracy": motionData?.magneticField.accuracy.rawValue.description ?? ""
+        ]
+        return motionArr
     }
 
     func prepareLocationData(locationData: CLLocation?) -> [String: String] {
@@ -138,4 +118,19 @@ struct LogItem {
             "timestamp": locationData?.timestamp.description ?? ""
         ]
     }
+
+    func prepareRotationMatrix(motionData: CMDeviceMotion?) -> [String: String] {
+        return [
+            "m1.1": motionData?.attitude.rotationMatrix.m11.description ?? "",
+            "m1.2": motionData?.attitude.rotationMatrix.m12.description ?? "",
+            "m1.3": motionData?.attitude.rotationMatrix.m13.description ?? "",
+            "m2.1": motionData?.attitude.rotationMatrix.m21.description ?? "",
+            "m2.2": motionData?.attitude.rotationMatrix.m22.description ?? "",
+            "m2.3": motionData?.attitude.rotationMatrix.m23.description ?? "",
+            "m3.1": motionData?.attitude.rotationMatrix.m31.description ?? "",
+            "m3.2": motionData?.attitude.rotationMatrix.m32.description ?? "",
+            "m3.3": motionData?.attitude.rotationMatrix.m33.description ?? ""
+        ]
+    }
 }
+
